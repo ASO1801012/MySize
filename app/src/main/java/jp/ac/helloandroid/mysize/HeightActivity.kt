87a409e5f14b4cc0fd6b283ded537b2d0f622755
ASO1801012 <1801012@s.asojuku.ac.jp>
2019-11-13 12:39:18 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/helloandroid/mysize/HeightActivity.kt
@@ -0,0 +1,12 @@
+package jp.ac.helloandroid.mysize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HeightActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_height)
+    }
+}
